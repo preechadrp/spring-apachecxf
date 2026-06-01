ea34bbd35fbe3be4882cd2e9fe81eeefc23c01ad
H P <H P@LAPTOP-KASON>
2026-06-01 23:34:58 +0700

    update

--- src/main/java/com/example/soap/service/HelloRequest.java
+++ src/main/java/com/example/soap/service/HelloRequest.java
@@ -1,5 +1,6 @@
 package com.example.soap.service;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
@@ -8,6 +9,7 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class HelloRequest {
 
+	@NotBlank
 	private String name;
 
 	public String getName() {
