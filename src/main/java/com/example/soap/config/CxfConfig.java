d7428b0638831227308ffc4133a29e41f657f1c0
H P <H P@LAPTOP-KASON>
2026-06-03 11:58:39 +0700

    update

--- src/main/java/com/example/soap/config/CxfConfig.java
+++ src/main/java/com/example/soap/config/CxfConfig.java
@@ -6,7 +6,6 @@ import org.apache.cxf.ext.logging.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import com.example.soap.service.AuthInterceptor;
 import com.example.soap.service.HelloServiceImpl;
@@ -14,13 +13,17 @@ import com.example.soap.service.HelloServiceImpl;
 @Configuration
 public class CxfConfig {
 
-	@Bean
-	@DependsOn(Bus.DEFAULT_BUS_ID)
-	jakarta.xml.ws.Endpoint helloEndpoint(
-			Bus bus,
-			HelloServiceImpl service) {
+	private final Bus bus;
 
-		EndpointImpl endpoint = new EndpointImpl(bus, service);
+	public CxfConfig(Bus bus) {
+		System.out.println("CxfConfig initialized");
+		this.bus = bus;
+	}
+
+	@Bean
+	jakarta.xml.ws.Endpoint helloEndpoint(HelloServiceImpl service) {
+
+		EndpointImpl endpoint = new EndpointImpl(this.bus, service);
 
 		endpoint.publish("/hello");
 
